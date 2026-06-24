5dd253f186cc90cd58f666c6b076cca30ba4b4cd
EngelsMedina <engelsmedina28@gmail.com>
2026-06-24 13:43:36 -0600

    Agregar proyecto.py y proyecto.md

---
+++ proyecto.md
@@ -0,0 +1,7 @@
+Nombre: Engels Manuel
+Apellido: Medina Baca
+Grupo: grupo 1
+Año: 2026
+Fecha: 24/06/2026
+Hora: 1:30pm
+
---
+++ proyecto.py
@@ -0,0 +1,6 @@
+# Nombre: Engels Manuel
+# Apellidos: Medina Baca
+# Grupo: grupo 1
+# Año: 2026
+# Fecha: 24/06/2026
+# Hora: 1:30pm
